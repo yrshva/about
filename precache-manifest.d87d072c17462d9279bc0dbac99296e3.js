f1bf2779a8313504ed1400b831e2fa6aacbbac96
jesjonesc <58461507+yrshva@users.noreply.github.com>
2022-10-16 01:38:26 +0100

    Updates

---
+++ precache-manifest.d87d072c17462d9279bc0dbac99296e3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "ad612f61a6e7a9830a05",
+    "url": "/about/static/css/main.9e5ddf64.chunk.css"
+  },
+  {
+    "revision": "ad612f61a6e7a9830a05",
+    "url": "/about/static/js/main.c26d54b1.chunk.js"
+  },
+  {
+    "revision": "628968930ce1f13d5cd8",
+    "url": "/about/static/js/runtime~main.8626ecb6.js"
+  },
+  {
+    "revision": "0f18623979ca6244eff7",
+    "url": "/about/static/css/2.1575694b.chunk.css"
+  },
+  {
+    "revision": "0f18623979ca6244eff7",
+    "url": "/about/static/js/2.f6dc624a.chunk.js"
+  },
+  {
+    "revision": "5c4c49af61a1d073ed44",
+    "url": "/about/static/js/3.141adcf5.chunk.js"
+  },
+  {
+    "revision": "9494dd46c73a60187eaaf5f118e6f90c",
+    "url": "/about/static/media/weather-app.9494dd46.png"
+  },
+  {
+    "revision": "5021c8fb243e31e647b91db731c37a4a",
+    "url": "/about/static/media/dictionary-app.5021c8fb.png"
+  },
+  {
+    "revision": "053d388c5bb7846754ecb118a7eeb27c",
+    "url": "/about/static/media/collection-services.053d388c.mp4"
+  },
+  {
+    "revision": "a7e1ff93657924fb36daa91809afbc10",
+    "url": "/about/static/media/mobile-dictionary.a7e1ff93.mp4"
+  },
+  {
+    "revision": "4da55444ebd09b11b7a43da8040d7c6c",
+    "url": "/about/static/media/tournament-app.4da55444.png"
+  },
+  {
+    "revision": "e28158910d162d053c2d9ca465bd4800",
+    "url": "/about/static/media/applications-app.e2815891.mp4"
+  },
+  {
+    "revision": "914bdee08c53d1e63b0f1867be5c6504",
+    "url": "/about/index.html"
+  }
+];
